866fa57b37e1eef5ad76729df55e98fd160439fb
BryanBlack523 <badboy523@mail.ru>
2021-10-28 22:37:03 +0900

    add basic spawn logic

---
+++ Assets/Scripts/Core/SpawnManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private float _spawnRate = 5f;
+    [SerializeField] private GameObject _enemyPrefab;
+
+    private bool _stopSpawning = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void StartSpawning()
+    {
+        StartCoroutine(SpawnEnemyRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnEnemyRoutine()
+    {
+        yield return new WaitForSeconds(3f);
+
+        while (!_stopSpawning)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(0f, 1f), 7, 0);
+            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _enemyPrefab.transform;
+
+            yield return new WaitForSeconds(_spawnRate);
+        }
+    }
+
+    public void OnPlayerDeath()
+    {
+        _stopSpawning = true;
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] float maxSpeed = 6f;
+    [SerializeField] Transform target;
+    [SerializeField] float health = 1;
+
+    private NavMeshAgent navMeshAgent;
+    private bool isDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.enabled = !isDead;// false when dead
+        MoveTo(target.position);
+
+        if (target.position.x == this.transform.position.x)
+        {
+            Death();
+        }
+    }
+
+    public void MoveTo(Vector3 destination)
+    {
+        navMeshAgent.destination = destination;
+        navMeshAgent.isStopped = false;
+    }
+
+    public void Death()
+    {
+        navMeshAgent.isStopped = true;
+        isDead = true;
+        health = 0;
+    }
+}
